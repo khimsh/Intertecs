4705e0489f9017666f0951b18a293f2a5734e564
Ia Khimsh <i.khimsh@gmail.com>
2018-02-16 22:05:53 +0400

    started fixing bugs

---
+++ source/js/mobile-menu.js
@@ -0,0 +1,26 @@
+// /*
+// * Navigation slideout and burger icon transform
+// */
+// var burger = document.querySelector('.burger-icon');
+// var navMenu = document.querySelector('.navigations-wrapper');
+// var navLink = document.querySelectorAll('.nav-link');
+// var logo = document.querySelector('.header-icon');
+
+// burger.addEventListener('click', function(){
+//   toggleMenu();
+// });
+
+// /*
+// * Closes the navigation on click
+// */
+// navLink.forEach(function (element){
+//   element.addEventListener("click", function(){
+//     toggleMenu();
+//   });
+// });
+
+// function toggleMenu() {
+//   burger.classList.toggle('closed');
+//   navMenu.classList.toggle('active');
+//   logo.classList.toggle('active');
+// }
