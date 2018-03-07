a76773a858fc34520393276bc179f2869ef9065b
khimsh <i.khimish@gmail.com>
2018-03-07 16:56:22 +0400

    added pop-up

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,29 +1,27 @@
-/*
-* Navigation slideout and burger icon transform
-*/
+// Navigation slideout and burger icon transform
 var burger = document.querySelector('.burger-icon');
 var navMenu = document.querySelector('#navigation');
-var navLink = document.querySelectorAll('.link');
+var navLink = document.getElementsByClassName('link');
 
+// Toggles Mobile Menu on click
 burger.addEventListener('click', function(){
   toggleMenu();
 });
 
-/*
-* Closes the navigation on click
-*/
-navLink.forEach(function (element){
-  element.addEventListener("click", function(){
-    toggleMenu();
-  });
-});
+// Closes the navigation on click
+// This is a workaround for IE
+for (var i in navLink) {
+  navLink[i].onclick = function(){toggleMenu();};
+}
 
+// Toggles Mobile Menu Classes
 function toggleMenu() {
   burger.classList.toggle('closed');
   navMenu.classList.toggle('active');
 }
 
-var wobble = document.querySelector('.pulse');
-wobble.addEventListener("click", function() {
-  wobble.classList.remove("pulse");
-});
+// Removes Pulse class from Nav Link on Click
+// var wobble = document.querySelector('.pulse');
+// wobble.addEventListener("click", function() {
+//   wobble.classList.remove("pulse");
+// });
---
+++ source/js/pop-up.js
@@ -0,0 +1,28 @@
+import { toUnicode } from "punycode";
+
+// JS for displaying Pop-up
+var popUp = document.getElementById('pop-up');
+var btnClose = document.getElementById('btn-close');
+var btnRedirect = document.getElementById('btn-redirect');
+
+// Adds className 'active' on page load
+document.addEventListener('DOMContentLoaded', function() {
+  popUp.className += ' active';
+}, true);
+
+// Removes className 'active, when x is clicked
+btnClose.addEventListener("click", function() {
+  popUp.classList.remove('active');
+});
+
+// Removes className 'active, when Pop-up call to action link is clicked
+btnRedirect.addEventListener("click", function() {
+  popUp.classList.remove('active');
+});
+
+// Closes Pop-up when the area outside of the Pop-up is clicked
+window.onclick = function(event) {
+  if (event.target == popUp) {
+    popUp.classList.remove('active');
+  }
+};
